c4ba216f729255d57823f2e7825c85cdfc6288b3
unknown <chaimaaglouib@gmail.com>
2025-04-27 17:46:42 +0100

    first commit

--- src/app/i/tasks/hooks/useTaskDebounce.ts
+++ src/app/i/tasks/hooks/useTaskDebounce.ts
@@ -1,48 +1,49 @@
-import { TypeTaskFormState } from "@/types/task.types";
-import { watch } from "fs";
-import debounce from "lodash.debounce";
-import { useCallback, useEffect } from "react";
-import useCreateTask from "./useCreateTask";
-import useUpdateTask from "./useUpdateTask";
-import { UseFormWatch } from "react-hook-form";
+import debounce from 'lodash.debounce'
+import { useCallback, useEffect } from 'react'
+import { UseFormWatch } from 'react-hook-form'
+
+import { TypeTaskFormState } from '@/types/task.types'
+
+import useCreateTask from './useCreateTask'
+import useUpdateTask from './useUpdateTask'
 
 interface IUseTaskDebounce {
-    watch: UseFormWatch<TypeTaskFormState>
-    itemId: string
+	watch: UseFormWatch<TypeTaskFormState>
+	itemId: string
 }
 
-export function useTaskDebounce({watch, itemId}: IUseTaskDebounce) {
-    const{createTask} = useCreateTask()
-    const{updateTask} = useUpdateTask()
+export function useTaskDebounce({ watch, itemId }: IUseTaskDebounce) {
+	const { createTask } = useCreateTask()
+	const { updateTask } = useUpdateTask()
 
-    const debouncedCreateTask = useCallback(
-        debounce((formData: TypeTaskFormState) => {
-            createTask(formData)
-        }, 444),
-        []
-    )
+	const debouncedCreateTask = useCallback(
+		debounce((formData: TypeTaskFormState) => {
+			createTask(formData)
+		}, 444),
+		[]
+	)
 
-    const debouncedUpdateTask = useCallback(
-        debounce((formData: TypeTaskFormState) => {
-            updateTask({id:itemId, data:formData})
-        }, 444),
-        []
-    )
+	const debouncedUpdateTask = useCallback(
+		debounce((formData: TypeTaskFormState) => {
+			updateTask({ id: itemId, data: formData })
+		}, 444),
+		[]
+	)
 
-    useEffect(()=>{
-        const { unsubscribe } = watch(formData => {
-            if (itemId) {
-                debouncedUpdateTask({
-                    ...formData,
-                    priority: formData.priority || undefined
-                })
-            } else {
-                debouncedCreateTask(formData)
-            }
-        })
+	useEffect(() => {
+		const { unsubscribe } = watch(formData => {
+			if (itemId) {
+				debouncedUpdateTask({
+					...formData,
+					priority: formData.priority || undefined
+				})
+			} else {
+				debouncedCreateTask(formData)
+			}
+		})
 
-        return () => {
-            unsubscribe()
-        }
-    }, [watch(), debouncedUpdateTask, debouncedCreateTask])
-}    
+		return () => {
+			unsubscribe()
+		}
+	}, [watch(), debouncedUpdateTask, debouncedCreateTask])
+}
