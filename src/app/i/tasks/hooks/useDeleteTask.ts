5a5294bc597eaefca6416f56122892e56856884e
unknown <chaimaaglouib@gmail.com>
2025-04-27 17:56:24 +0100

    first commit

--- src/app/i/tasks/hooks/useDeleteTask.ts
+++ src/app/i/tasks/hooks/useDeleteTask.ts
@@ -1,23 +1,21 @@
-import { taskService } from '@/services/task.service'
-import { TypeTaskFormState } from '@/types/task.types'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 
+import { taskService } from '@/services/task.service'
+
 const useDeleteTask = () => {
-  const queryClient = useQueryClient()
+	const queryClient = useQueryClient()
 
-  const { mutate: deleteTask, isPending: isDeletePending } = useMutation({
-    mutationKey: ['delete task'],
-    mutationFn: ( id: string ) => taskService.deleteTask(id),
-    onSuccess() {
-      queryClient.invalidateQueries({
-        queryKey: ['tasks']
-      })
-    }
-  })
-  
-  return { deleteTask, isDeletePending }
+	const { mutate: deleteTask, isPending: isDeletePending } = useMutation({
+		mutationKey: ['delete task'],
+		mutationFn: (id: string) => taskService.deleteTask(id),
+		onSuccess() {
+			queryClient.invalidateQueries({
+				queryKey: ['tasks']
+			})
+		}
+	})
+
+	return { deleteTask, isDeletePending }
 }
-
-
 
 export default useDeleteTask
--- src/components/ui/fields/Fields.tsx
+++ src/components/ui/fields/Fields.tsx
@@ -1,13 +1,11 @@
-import { ChangeEventHandler, forwardRef } from "react";
-import { FieldError } from "react-hook-form";
+import { forwardRef } from 'react'
+import { FieldError } from 'react-hook-form'
 
-//alredady have "InputFieldProps". 
+//alredady have "InputFieldProps".
 // wants to extend InputFieldProps(Declaration Merging) in type didn't have merging.
 //interface cannot be used for unions.
 
-
 //1.type is better when defining a set of possible values. type Status = "loading" | "success" | "error";(Union Types)
-
 
 /** 2. Mapped Types (Dynamic Property Creation)
 Mapped types allow you to create new type dynamically based on an existing one.
@@ -50,8 +48,6 @@ type UserPreview = {
 };
 */
 
-
-
 //A function type can define a function signature as a variable or property .
 
 //✅ Use method signatures (makeSound(): void;) when working with objects or classes that require this.
@@ -65,7 +61,6 @@ interface Animal = {
     makeSound(): void; // Method signature, no implementation
 }
 */
-
 
 // interface Person {
 //     name: string;
@@ -84,62 +79,61 @@ interface Animal = {
 // person.greet();
 
 interface InputFieldProps {
-    id: string;
-    label: string;
-    extra?: string;
-    placeholder: string;
-    variant?: string;
-    state?: 'error' | 'success';
-    disabled?: boolean;
-    type?: string;
-    isNumber?: boolean;
-    error:FieldError | undefined
+	id: string
+	label: string
+	extra?: string
+	placeholder: string
+	variant?: string
+	state?: 'error' | 'success'
+	disabled?: boolean
+	type?: string
+	isNumber?: boolean
+	error: FieldError | undefined
 }
 // interface User1 {
 //     name: string;
 //     age?: number; // 'age' is optional
 //   }
-  
+
 //   interface User2 {
 //     name: string;
 //     age: number | undefined; // 'age' must exist but can be 'undefined'
 //   }
-  
+
 //   const user1: User1 = { name: "Alice" }; // Valid: 'age' is optional
 //   const user2: User2 = { name: "Bob", age: undefined }; // Valid: 'age' is explicitly 'undefined'
 
-
 export const Field = forwardRef<HTMLInputElement, InputFieldProps>(
-    (
-        {
-            label,
-            id,
-            extra,
-            type,
-            placeholder,
-            state,
-            disabled,
-            isNumber,
-            error,
-            ...rest
-        },
-        ref
-    ) => {
-        return (
-            <div className={`${extra}`}>
-                <label
-                    htmlFor={id}
-                    className={`text-sm text-white/60 dark:text-white ml-1.5 font-medium`}
-                >
-                    {label}
-                </label>
-                <input
-                    ref={ref}
-                    disabled={disabled}
-                    type={type}
-                    id={id}
-                    placeholder={placeholder}
-                    className={`
+	(
+		{
+			label,
+			id,
+			extra,
+			type,
+			placeholder,
+			state,
+			disabled,
+			isNumber,
+			error,
+			...rest
+		},
+		ref
+	) => {
+		return (
+			<div className={`${extra}`}>
+				<label
+					htmlFor={id}
+					className={`text-sm text-white/60 dark:text-white ml-1.5 font-medium`}
+				>
+					{label}
+				</label>
+				<input
+					ref={ref}
+					disabled={disabled}
+					type={type}
+					id={id}
+					placeholder={placeholder}
+					className={`
                         mt-2
                         flex
                         w-full
@@ -157,34 +151,36 @@ export const Field = forwardRef<HTMLInputElement, InputFieldProps>(
                         duration-500
                         transition-colors
                         focus:border-primary
-                        ${disabled === true
-                            ? '!border-none !bg-gray-100 dark:!bg-white/5 dark:placeholder:!text-[rgba(255, 255, 255, 0.15)]'
-                            : state === 'error'
-                                ? 'border-red-500 text-red-500 placeholder:text-red-500 dark:!border-red-400 dark:!text-red-400 dark:placeholder:!text-red-400'
-                                : state === 'success'
-                                    ? 'border-green-500 text-green-500 placeholder:text-green-500 dark:!border-green-400 dark:!text-green-400 dark:placeholder:!text-green-400'
-                                    : ''
-                        }
+                        ${
+													disabled === true
+														? '!border-none !bg-gray-100 dark:!bg-white/5 dark:placeholder:!text-[rgba(255, 255, 255, 0.15)]'
+														: state === 'error'
+															? 'border-red-500 text-red-500 placeholder:text-red-500 dark:!border-red-400 dark:!text-red-400 dark:placeholder:!text-red-400'
+															: state === 'success'
+																? 'border-green-500 text-green-500 placeholder:text-green-500 dark:!border-green-400 dark:!text-green-400 dark:placeholder:!text-green-400'
+																: ''
+												}
                     `}
-                    onKeyDown={(event) => {
-                        if (
-                            isNumber &&
-                            !/[0-9]/.test(event.key) &&
-                            event.key !== 'Backspace' &&
-                            event.key !== 'Tab' &&
-                            event.key !== 'Enter' &&
-                            event.key !== 'ArrowLeft' &&
-                            event.key !== 'ArrowRight'
-                        ) {
-                            event.preventDefault();
-                        }
-                    }}
-                    {...rest}
-                />
-                <p className="text-red-500">{error?.message}</p>  {/* Displaying error */}
-                </div>
-        );
-    }
+					onKeyDown={event => {
+						if (
+							isNumber &&
+							!/[0-9]/.test(event.key) &&
+							event.key !== 'Backspace' &&
+							event.key !== 'Tab' &&
+							event.key !== 'Enter' &&
+							event.key !== 'ArrowLeft' &&
+							event.key !== 'ArrowRight'
+						) {
+							event.preventDefault()
+						}
+					}}
+					{...rest}
+				/>
+				<p className='text-red-500'>{error?.message}</p>{' '}
+				{/* Displaying error */}
+			</div>
+		)
+	}
 )
 
 Field.displayName = 'field'
