f6b86e28db88a780f1db3b2fab72c74a1ed145f0
unknown <chaimaaglouib@gmail.com>
2025-04-28 08:54:23 +0100

    first commit

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -1,51 +1,56 @@
-import { getAccessToken, removeFromStorage } from "@/services/auth-token.service";
-import axios, { CreateAxiosDefaults } from "axios";
-import { errorCatch } from "./error";
-import { authService } from "@/services/auth.service";
+import axios, { CreateAxiosDefaults } from 'axios'
+
+import { errorCatch } from './error'
+import {
+	getAccessToken,
+	removeFromStorage
+} from '@/services/auth-token.service'
+import { authService } from '@/services/auth.service'
 
 const options: CreateAxiosDefaults = {
-    baseURL: 'http://localhost:4200/api',
-    headers: {
-        "Content-Type": "application/json",
-    },
-    withCredentials: true
-};
+	baseURL: process.env.NEXT_PUBLIC_API_URL,
+	headers: {
+		'Content-Type': 'application/json'
+	},
+	withCredentials: true
+}
 
 const axiosClassic = axios.create(options)
 
 const axiosWithAuth = axios.create(options)
 
 axiosWithAuth.interceptors.request.use(config => {
-    const accessToken = getAccessToken()
+	const accessToken = getAccessToken()
 
-    if (config?.headers && accessToken)
-        config.headers.Authorization = `Bearer ${accessToken}`
+	if (config?.headers && accessToken)
+		config.headers.Authorization = `Bearer ${accessToken}`
 
-    return config
+	return config
 })
 
 axiosWithAuth.interceptors.response.use(
-    config => config,
-    async error => {
-        const originalRequest = error.config
-console.log(error)
-        if (
-            (error?.status === 401 ||
-            errorCatch(error) === 'jwt expired' ||
-            errorCatch(error) === 'jwt must be provided') && error.config && !error.config._isRetry      
-        )
-        {
-            originalRequest._isRetry = true
-            try {
-                await authService.getNewTokens()
-                return axiosWithAuth.request(originalRequest)                
-            } catch (error) {
-                if(errorCatch(error) === 'jwt expired') removeFromStorage()                    
-            }
-        }
+	config => config,
+	async error => {
+		const originalRequest = error.config
+		console.log(error)
+		if (
+			(error?.status === 401 ||
+				errorCatch(error) === 'jwt expired' ||
+				errorCatch(error) === 'jwt must be provided') &&
+			error.config &&
+			!error.config._isRetry
+		) {
+			originalRequest._isRetry = true
+			try {
+				await authService.getNewTokens()
+				return axiosWithAuth.request(originalRequest)
+			} catch (error) {
+				if (errorCatch(error) === 'jwt expired') removeFromStorage()
+			}
+		}
 
-    throw error
-})
+		throw error
+	}
+)
 
-export { axiosClassic, axiosWithAuth}
-
+export { axiosClassic, axiosWithAuth }
