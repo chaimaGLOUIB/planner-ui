fbe096284bfb7966447b9c96f4c0e9b0fcaa3e47
unknown <chaimaaglouib@gmail.com>
2025-04-30 13:49:03 +0100

    update cookie settings for environment-specific

--- src/app/auth/Auth.tsx
+++ src/app/auth/Auth.tsx
@@ -1,110 +1,117 @@
 'use client'
 
-import { Button } from "@/components/ui/buttons/Button"
-import { Field } from "@/components/ui/fields/Fields"
-import { Heading } from "@/components/ui/Heading"
-import { DASHBOARD_PAGES } from "@/config/pages-url.config"
-import { authService } from "@/services/auth.service"
-import { IAuthForm } from "@/types/auth.types"
-import { useMutation } from "@tanstack/react-query"
-import { useRouter } from "next/navigation"
-import { useState } from "react"
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
-import { toast } from "sonner"
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+import { Heading } from '@/components/ui/Heading'
+import Loader from '@/components/ui/Loader'
+import { Button } from '@/components/ui/buttons/Button'
+import { Field } from '@/components/ui/fields/Fields'
+
+import { IAuthForm } from '@/types/auth.types'
+
+import { DASHBOARD_PAGES } from '@/config/pages-url.config'
 
 
-export function Auth() {
- 
-    const [isLoginForm, setIsLoginForm] = useState(false)
+import { authService } from '@/services/auth.service'
 
-    const { push } = useRouter()
-
-    const { mutate } = useMutation({
-        mutationKey: ['auth'],
-        mutationFn: (data: IAuthForm) => authService.main(isLoginForm ? 'login' :
-            'register', data),
-        onSuccess() {
-            toast("Successfully login!", {
-                style: {
-                  backgroundColor: 'green',
-                  
-                },
-                duration: 5000, // Duration in milliseconds (5000ms = 5 seconds)
-              });
-            reset()        
-            push(DASHBOARD_PAGES.HOME)
-        },
-
-        onError(error) {
-
-            toast(error.message, {
-                style: {
-                  backgroundColor: 'red',
-                  
-                },
-                duration: 5000, // Duration in milliseconds (5000ms = 5 seconds)
-              });
-            }
-            
-        }
-
-    )
-    const onSubmit: SubmitHandler<IAuthForm> = data => {
-        mutate(data)
-    }
-    const userSchema = z.object({
-        email: z.string().email("Invalid email address"),
-        password: z.string().min(8, "Password must be at least 8 characters long"),
-    });
-    
+const FullScreenLoader = () => (
+	<div className="fixed inset-0 z-50 bg-black bg-opacity-50 flex items-center justify-center">
+	  <div className="w-12 h-12 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+	</div>
+  );
   
-    const {
-        register,
-        handleSubmit,
-        reset, // Import reset method from react-hook-form
-        formState: { errors },   //formState: {
-                                //     isDirty: false,
-                                //     isValid: true,
-                                //     errors: { /* error details */ },
-                                //   }
-      } = useForm<IAuthForm>({
-        resolver: zodResolver(userSchema),
-      });
-    return (
-        <div className='flex min-h-screen'>
-            <form
-                className='w-1/4 m-auto shadow bg-sidebar rounded-xl p-layout '
-                onSubmit={handleSubmit(onSubmit)}
-            >
-                <Heading title='Auth' />
+export function Auth() {
+	const [isLoginForm, setIsLoginForm] = useState(false)
 
-                <Field
-                    id="email"
-                    label="Email"
-                    placeholder="Enter email:"
-                    type="email"
-                    extra='mb-4'                   
-                    {...register("email")}
-                    error={errors.email}                    
-                />
+	const { push } = useRouter()
 
-                <Field
-                    id="password"
-                    label="Password"
-                    placeholder="Enter password:"
-                    type="password"
-                    extra='mb-6'
-                    {...register("password")}
-                    error={errors.password}                        
-                />
+	const { mutate, isPending } = useMutation({
+		mutationKey: ['auth'],
+		mutationFn: (data: IAuthForm) =>
+			authService.main(isLoginForm ? 'login' : 'register', data),
+		onSuccess() {
+			toast('Successfully login!', {
+				style: {
+					backgroundColor: 'green'
+				},
+				duration: 5000 // Duration in milliseconds (5000ms = 5 seconds)
+			})
+			reset()
+			push(DASHBOARD_PAGES.HOME)
+		},
 
-                <div className="flex items-center gap-5 justify-center">
-                    <Button onClick={()=> setIsLoginForm(true)}>Login</Button>
-                    <Button onClick={()=> setIsLoginForm(false)}>Register</Button>
-                </div>
-            </form>
-        </div>
-    )
+		onError(error) {
+			toast(error.message, {
+				style: {
+					backgroundColor: 'red'
+				},
+				duration: 5000 // Duration in milliseconds (5000ms = 5 seconds)
+			})
+		}
+	})
+	const onSubmit: SubmitHandler<IAuthForm> = data => {
+		mutate(data)
+	}
+	const userSchema = z.object({
+		email: z.string().email('Invalid email address'),
+		password: z.string().min(8, 'Password must be at least 8 characters long')
+	})
+
+	const {
+		register,
+		handleSubmit,
+		reset, // Import reset method from react-hook-form
+		formState: { errors } //formState: {
+		//     isDirty: false,
+		//     isValid: true,
+		//     errors: { /* error details */ },
+		//   }
+	} = useForm<IAuthForm>({
+		resolver: zodResolver(userSchema)
+	})
+
+	return (
+		<div className="flex min-h-screen relative">
+  {isPending && <FullScreenLoader />}
+
+  <form
+    className="w-1/4 m-auto shadow bg-sidebar rounded-xl p-layout"
+    onSubmit={handleSubmit(onSubmit)}
+  >
+    <Heading title="Auth" />
+
+    <Field
+      id="email"
+      label="Email"
+      placeholder="Enter email:"
+      type="email"
+      extra="mb-4"
+      {...register('email')}
+      error={errors.email}
+    />
+
+    <Field
+      id="password"
+      label="Password"
+      placeholder="Enter password:"
+      type="password"
+      extra="mb-6"
+      {...register('password')}
+      error={errors.password}
+    />
+
+    <div className="flex items-center gap-5 justify-center">
+      <Button onClick={() => setIsLoginForm(true)}>Login</Button>
+      <Button onClick={() => setIsLoginForm(false)}>Register</Button>
+    </div>
+  </form>
+</div>
+
+	)
 }
